24eff5481bc3bc016da8988c4791399d357bd053
lofic <louis.coilliot@gmail.com>
2012-06-20 21:00:28 +0200

    Use of yumrepo puppet resource type

--- modules/jboss/manifests/init.pp
+++ modules/jboss/manifests/init.pp
@@ -1,20 +1,19 @@
 class jboss($up = true) {
     $jbossver = '6.1.0.Final'
 
-    file { 'jboss.repo':
-        path   => '/etc/yum.repos.d/jboss.repo',
-        ensure => present,
-        source => 'puppet:///jboss/jboss.repo',
-        owner  => 'root',
-        group  => 'root',
-        mode   => 0644,
-    }
+    yumrepo { "jboss":
+       baseurl   =>
+         "http://www.kermit.fr/repo/rpm/el\$releasever/\$basearch/jboss/",
+       descr     => "JBoss",
+       enabled   => 1,
+       gpgcheck  => 0,
+    }    
 
     user{ 'jboss':
         comment      => 'jboss',
         ensure       => present,
         managehome   => true,
-        home         => '/opt/jboss'
+        home         => '/opt/jboss',
     }
 
     file { 'jboss_profile':
@@ -32,35 +31,17 @@ class jboss($up = true) {
     }
 
     package { 'jbossas':
-        ensure => present,
+        ensure  => present,
         require => Package['java-1.6.0-openjdk'],
     }
     
-    #package { 'jboss-as-distribution':
-    #    require => File['jboss.repo'],
-    #    ensure  => present,
-    #}
-
-    #package { 'unzip':
-    #    ensure => present,
-    #}
-
-    #exec { 'extract jboss':
-    #    require => [ User['jboss'], Package['jboss-as-distribution'], Package['unzip'], ],
-    #    command => "/usr/bin/unzip /opt/jboss-as-distribution/jboss-as-distribution-${jbossver}.zip -d /opt/jboss/",
-    #    creates => "/opt/jboss/jboss-${jbossver}/bin/probe.sh",
-    #    user    => 'jboss',
-    #    group   => 'jboss',
-    #    onlyif  => '/usr/bin/test -d /opt/jboss-as-distribution',
-    #}
-
     file { 'jboss_service':
         ensure  => present,
         path    => '/etc/init.d/jboss',
         content => template('jboss/jboss_service'),
         owner   => 'root',
         group   => 'root',
-        mode    => 0755,
+        mode    => '0755',
     }
 
     service{ 'jboss':
